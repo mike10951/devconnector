f6171b8f92ef604e6a76584b6779bb76ac40cbfb
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-02-21 09:43:17 -0600

    Retrieves github repos by user id

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,7 +17,7 @@ router.post(
     check('name', 'Name is required')
       .not()
       .isEmpty(),
-    check('email', 'Please incluede a valid email').isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
