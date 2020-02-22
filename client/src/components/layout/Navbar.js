5905218e30823aae59574e96bf2f12e586ea68ba
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-02-22 11:25:23 -0600

    Add users from react frontend

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => (
   <nav className='navbar bg-dark'>
     <h1>
-      <a href='index.html'>
-        <i className='fas fa-code'></i> DevConnector
-      </a>
+      <Link to='/'>
+        <FontAwesomeIcon icon={faCode} /> DevConnector
+      </Link>
     </h1>
     <ul>
       <li>
-        <a href='profiles.html'>Developers</a>
+        <Link to='!#'>Developers</Link>
       </li>
       <li>
-        <a href='register.html'>Register</a>
+        <Link to='/register'>Register</Link>
       </li>
       <li>
-        <a href='login.html'>Login</a>
+        <Link to='/login'>Login</Link>
       </li>
     </ul>
   </nav>
